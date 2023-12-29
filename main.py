8487eff272c39fc62b8703ebd416e8db8a60b7f6
Bhaskar Mahto <arbkm22@gmail.com>
2023-12-29 17:24:25 +0530

    added hashnode handler

--- main.py
+++ main.py
@@ -13,11 +13,18 @@ logging.basicConfig(
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="Welcome to WFH Jobs Bot")
 
+async def get_hashnode_jobs(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    jobs = hashnode()
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=jobs)
+
 
 if __name__ == "__main__":
     application = ApplicationBuilder().token(config.TOKEN).build()
     
     start_handler = CommandHandler('start', start)
     application.add_handler(start_handler)
+
+    hashnode_handler = CommandHandler('hashnode', get_hashnode_jobs)
+    application.add_handler(hashnode_handler)
 
     application.run_polling()
