6b3c0df8e202c76c2565dd4952bb5fbfe549bb22
Bhaskar Mahto <arbkm22@gmail.com>
2023-12-29 22:32:53 +0530

    - added dotenv file - updated gitignore

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+import os
+import json
 import logging
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from dotenv import load_dotenv
 
 import config
 from hashnode import hashnode
+
+load_dotenv()
+TG_TOKEN = os.getenv('TOKEN')
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(message)s',
@@ -17,14 +23,14 @@ async def get_hashnode_jobs(update: Update, context: ContextTypes.DEFAULT_TYPE):
     jobs = hashnode()
     await context.bot.send_message(chat_id=update.effective_chat.id, text=jobs)
 
+def get_user_preference(): 
+    print('get user preference')
 
 if __name__ == "__main__":
-    application = ApplicationBuilder().token(config.TOKEN).build()
-    
-    start_handler = CommandHandler('start', start)
-    application.add_handler(start_handler)
+    application = ApplicationBuilder().token(TG_TOKEN).build()
 
-    hashnode_handler = CommandHandler('hashnode', get_hashnode_jobs)
-    application.add_handler(hashnode_handler)
+    # handlers
+    application.add_handler(CommandHandler('start', start)) 
+    application.add_handler(CommandHandler('hashnode', get_hashnode_jobs))
 
     application.run_polling()
