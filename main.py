aed1088912a55a33f9801a02693bf3a75175ab5c
Bhaskar Mahto <arbkm22@gmail.com>
2023-12-29 15:56:32 +0530

    Created Telegram Bot

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
+import logging
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+
+import config
 from hashnode import hashnode
 
-def main():
-    print('hello, world!')
-    jobs = hashnode()
-    print(f'jobs: {jobs}')
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(message)s',
+    level=logging.INFO
+)
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Welcome to WFH Jobs Bot")
+
 
 if __name__ == "__main__":
-    main()
+    application = ApplicationBuilder().token(config.TOKEN).build()
+    
+    start_handler = CommandHandler('start', start)
+    application.add_handler(start_handler)
+
+    application.run_polling()
