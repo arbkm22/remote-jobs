bdfbc98810294a20b9f3a5589eb4c4e4e8859d20
Bhaskar Mahto <arbkm22@gmail.com>
2024-01-06 17:49:26 +0530

    user pref check set

--- main.py
+++ main.py
@@ -4,14 +4,13 @@ import logging
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, CallbackContext, Updater
 from dotenv import load_dotenv
-
+import datetime
 from hashnode import hashnode
 
 load_dotenv()
 TG_TOKEN = os.getenv('TOKEN')
 PREFERENCE_FILE = os.getenv('PREFERENCE_FILE', 'user_preference.json')
 
-# TODO: schedule the bot to run every day
 # TODO: send the users message
 
 def load_preference():
@@ -31,7 +30,7 @@ def save_preference(preferences):
     with open(PREFERENCE_FILE, 'w') as file:
         json.dump(preferences, file, indent=4)
 
-async def set_language(update: Update, context: CallbackContext):
+async def set_user_pref(update: Update, context: CallbackContext):
     user_inputs = context.args
     if (len(user_inputs) < 1):
         await update.message.reply_text("provide a language")
@@ -53,18 +52,12 @@ async def set_language(update: Update, context: CallbackContext):
     save_preference(preferences)
     await update.message.reply_text(f'preferred language: {context.args[0]}')
 
-async def get_language(update: Update, context: CallbackContext):
-    user_id = str(update.message.from_user.id)
+def get_user_pref(user_id):
     preferences = load_preference()
-    print(f'get lang pref: {preferences}')
-    print(f'get_lang user id: {user_id}')
+    user_id = str(user_id)
     language = preferences.get(user_id)
-    print(f'language: {language}')
-    if language:
-        await update.message.reply_text(f'preferred language: {language}')
-    else:
-        await update.message.reply_text(f'You do not have any preferred language set')
-
+    print(f'lang in get_user_pref: {language}')
+    return language
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(message)s',
@@ -77,15 +70,47 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 async def get_hashnode_jobs(update: Update, context: ContextTypes.DEFAULT_TYPE):
     jobs = hashnode()
     await context.bot.send_message(chat_id=update.effective_chat.id, text=jobs)
+    return jobs
 
 async def callback_alarm(context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=context.job.chat_id, text=f'BEEP {context.job.data}')
+    jobs = hashnode()
+    user_id = context.job.user_id
+    print(f'user_id: {user_id}')
+    langs = get_user_pref(user_id)
+    if jobs is None:
+        await context.bot.send_message(chat_id=context.job.chat_id, text='There are no jobs')
+    elif langs is None:
+        await context.bot.send_message(
+            chat_id=context.job.chat_id, 
+            text='No preferrence set. Please set a preference'
+        )
+    else:
+        for lang in langs:
+            if lang in jobs:
+                await context.bot.send_message(
+                    chat_id=context.job.chat_id,
+                    text=f'{lang} keyword found'
+                )
+            else:
+                await context.bot.send_message(
+                    chat_id=context.job.chat_id,
+                    text='no keyword found'
+                )
 
 async def callback_timer(update: Update, context: ContextTypes.DEFAULT_TYPE):
     chat_id = update.message.chat_id
+    user_id = update.message.from_user.id
     name = update.effective_chat.full_name
     # 8 hours = 28800 seconds
-    context.job_queue.run_repeating(callback_alarm, interval=28800, first=1, data=name, chat_id=chat_id)
+    context.job_queue.run_repeating(
+        callback_alarm, 
+        interval=28800, 
+        first=1, 
+        data=name, 
+        chat_id=chat_id, 
+        user_id=user_id
+    )
 
 def main() -> None:
     application = ApplicationBuilder().token(TG_TOKEN).build()
@@ -93,8 +118,8 @@ def main() -> None:
     # handlers
     application.add_handler(CommandHandler('start', start))
     application.add_handler(CommandHandler('hashnode', get_hashnode_jobs))
-    application.add_handler(CommandHandler('set_lang', set_language))
-    application.add_handler(CommandHandler('get_lang', get_language))
+    application.add_handler(CommandHandler('set_pref', set_user_pref))
+    # application.add_handler(CommandHandler('get_pref', get_user_pref))
     application.add_handler(CommandHandler('timer', callback_timer))
 
     application.run_polling()
