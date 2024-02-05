6ff65bb621eb269c6ed30da4177120758d67a32b
Bhaskar Mahto <arbkm22@gmail.com>
2024-02-05 09:57:47 +0530

    extracted jobs from the site

--- openplay.py
+++ openplay.py
@@ -8,8 +8,26 @@ OPENPLAY_URL = 'https://openplay.co/careers/'
 def openplay():
     driver = webdriver.Chrome()
     driver.get(OPENPLAY_URL)
-    page_source = driver.page_source
-    print(f'page source: ${page_source}')
+    time.sleep(3)
+    curr_pos = driver.find_elements(By.CLASS_NAME, "section")
+    jobs_element = []
+    for job in curr_pos:
+        if ('CURRENT POSITIONS' in job.text):
+            jobs_element = job.text.split('\n')
+
+    # TODO: create map of the jobs
+    job_map = {}
+    jobs_list = []
+    for ele in jobs_element:
+        if ele == 'CURRENT POSITIONS':
+            continue
+        elif ele == 'Name your dream job':
+            break
+        else:
+            jobs_list.append(ele)
+    
+    print(f'jobs: {jobs_list}')
+
 
 if __name__ == "__main__":
     openplay()
